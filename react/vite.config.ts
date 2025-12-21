49ef3b91e1386e6a399567ba1c7574a500851b3b
bri111 <brianall@hawaii.edu>
2025-12-20 15:18:03 -1000

    update

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -18,7 +18,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Spring-2026/',
+  base: '/react/',
   plugins: [
     react({
       babel: {
