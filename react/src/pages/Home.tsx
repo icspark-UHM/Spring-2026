cfd48f6153255b0f68276333444efacbeebd7014
bri111 <brianall@hawaii.edu>
2026-01-02 19:40:25 -1000

    push

--- react/src/pages/Home.tsx
+++ react/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import  classroom from "public/files/classroom.pdf";
+import  classroom from "/public/files/classroom.pdf";
 
 function Home() {
   return (
