7415140416ec5472b6d51166c6acc0483f704d6c
Ahron Natividad <ahron.natividad12@gmail.com>
2026-02-27 10:30:30 -1000

    Added Location and Parking

--- react/src/pages/Hackathon/Schedule.tsx
+++ react/src/pages/Hackathon/Schedule.tsx
@@ -16,6 +16,38 @@ function HackathonSchedule() {
         </div>
       </section>
 
+      <section className={styles.pageHeader}>
+        <div className={styles.container}>
+          <div className={styles.contentCard}>
+            <h1>Location and Parking</h1>
+            <div className={styles.expectationGrid}>
+              <div className={styles.expectationItem}>
+                <h2>Our Venue</h2>
+                <img src='imgs/hackathon/Parking.png' alt="Hackathon Venue" />
+
+                <h3> Welcome Room Campus Center 3rd Floor R307 </h3>
+                <h4> Subject to change building location. Will notify if happens </h4>
+                <h4><br /> 2465 Campus Rd, Honolulu, HI 96822 <br /> University of Hawaiʻi at Mānoa </h4>
+
+                {/* Add this Legend Overlay or List */}
+                <br />
+                <div className={styles.mapLegend}>
+                  <p>⭐ <strong>Star:</strong> Welcome Room (Campus Center, R307)</p>
+                  <p>📍 <strong>Red Pin:</strong> Recommended Parking </p>
+                </div>
+
+              </div>
+            </div>
+              <ul className={styles.objectivesList}>
+                <li>
+                  <strong>Parking Situation:</strong> Parking is free on weekends and State Holidays in permit-only and visitor lots except during Athletic and special events. 
+                  SWITCH and ICSpark are not liable for any mode of transportation chosen by the students or parent/guardian/coach to travel to and from the UH Mānoa campus.
+                </li>
+              </ul>
+          </div>
+        </div>
+      </section>
+     
       {/* Schedule Content */}
       <section className={styles.contentSection}>
         <div className={styles.container}>
